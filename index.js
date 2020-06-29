229e4a91653b9613a3646b2ddaa6500814c21519
iamhuynq <nguyenquanghuy0511@gmail.com>
2020-06-29 14:27:35 +0700

    Set up environment

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ const port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
 app.listen(port, () => console.log('Server is listening on port: ', port));
--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  tabWidth: 2,
-  semi: true,
-  singleQuote: true,
-  trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
 };
